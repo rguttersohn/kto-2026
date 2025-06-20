823390d536642b7b911d1bca8f9aabdda7f58699
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2025-06-20 08:45:12 -0400

    adds filtering to location based indicators

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Traits\HandlesAPIRequestOptions;
 use Illuminate\Support\Facades\Response;
 use App\Models\Location;
 use App\Support\StandardizeResponse;
@@ -10,7 +11,8 @@ use App\Models\Indicator;
 
 class LocationsController extends Controller
 {
-    
+    use HandlesAPIRequestOptions;
+
     public function getLocations(){
 
         $locations = Location::select('location_type_id','name','id','fips','geopolitical_id')->get();
@@ -96,26 +98,12 @@ class LocationsController extends Controller
 
     public function getLocationIndicatorData(Request $request, $location_id, $indicator_slug){
         
-        $timeframe = $request->has('timeframe') ? $request->timeframe : null;
-
-        $breakdown = $request->has('breakdown') ? $request->breakdown: null;
-
-        $data_format = $request->has('data_format') ? $request->data_format : null;
-
         $offset = $request->has('offset') ? $request->offset : 0;
 
         $limit = $request->has('limit') ? $request->limit : 3000;
 
-        $as = $request->has('as') ? $request->as : 'json';
-
-        $wants_geojson = false;
-
-        $accepts_geojson = str_contains($request->header('Accept'), 'application/geo+json');
-        
-        if($as === 'geojson' || $accepts_geojson) {
-            $wants_geojson = true;
-        }
-
+        $wants_geojson = $this->wantsGeoJSON($request);
+    
         $location = Location::select('id')
             ->where('id', $location_id)
             ->firstOrFail();
@@ -132,13 +120,10 @@ class LocationsController extends Controller
         $indicator_data = Indicator::select('id', 'slug', 'name')
             ->where('slug', $indicator_slug)
             ->withDataDetails(
-                breakdown: $breakdown,
-                timeframe: $timeframe,
-                data_format: $data_format,
-                offset: $offset,
                 limit: $limit,
+                offset: $offset,
                 wants_geojson: $wants_geojson,
-                location: $location->id
+                filters: $request->input('filter', [])
             )->get();
 
         if($wants_geojson){
