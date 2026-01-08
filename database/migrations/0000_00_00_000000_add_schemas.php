a68d6d1a136a657d5ad5570e617bc2593834f642
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-08 16:37:59 -0500

    removes lines creating unused schemas

--- database/migrations/0000_00_00_000000_add_schemas.php
+++ database/migrations/0000_00_00_000000_add_schemas.php
@@ -12,11 +12,9 @@ return new class extends Migration
     {   
         DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS app');
         DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS users');
-        DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS assets');
         DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS locations');
         DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS indicators');
         DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS domains');
-        DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS collections');
         DB::connection('supabase')->statement('CREATE SCHEMA IF NOT EXISTS migrations');
     }
 
@@ -26,11 +24,9 @@ return new class extends Migration
     public function down(): void
     {   
         DB::connection('supabase')->statement('DROP SCHEMA IF EXISTS users CASCADE');
-        DB::connection('supabase')->statement('DROP SCHEMA IF EXISTS assets CASCADE');
         DB::connection('supabase')->statement('DROP SCHEMA IF EXISTS locations CASCADE');
         DB::connection('supabase')->statement('DROP SCHEMA IF EXISTS indicators CASCADE');
         DB::connection('supabase')->statement('DROP SCHEMA IF EXISTS domains CASCADE');
-        DB::connection('supabase')->statement('DROP SCHEMA IF EXISTS collections CASCADE');
         DB::connection('supabase')->statement('DROP SCHEMA IF EXISTS migrations CASCADE');
 
     }
