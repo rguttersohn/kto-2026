fd100c07732f949e86a0a05be43fe1cc0c51a4c2
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-09 12:47:14 -0500

    uses indicator data collection to return geojson properties

--- app/Http/Resources/IndicatorGeoJSONDataResource.php
+++ app/Http/Resources/IndicatorGeoJSONDataResource.php
@@ -9,25 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class IndicatorGeoJSONDataResource extends JsonResource
 {
 
-    protected function getDataAsGeoJSON(Model $indicator){
-
-        return [
-            'type' => 'Feature',
-            'geometry' => json_decode($this->geometry),
-            'properties' => [
-                "data" => $this->data,
-                "indicator_id" => $this->indicator_id,
-                "location_id" => $this->location_id,
-                "location" => $this->location,
-                "location_type_id" => $this->location_type_id,
-                "location_type" => $this->location_type,
-                "timeframe" => $this->timeframe,
-                "breakdown" => $this->breakdown_name,
-                "format" => $this->format
-            ]
-        ];
-        
-    }
+    
 
     /**
      * Transform the resource into an array.
@@ -40,17 +22,7 @@ class IndicatorGeoJSONDataResource extends JsonResource
         return [
             'type' => 'Feature',
             'geometry' => json_decode($this->geometry),
-            'properties' => [
-                "data" => $this->data,
-                "indicator_id" => $this->indicator_id,
-                "location_id" => $this->location_id,
-                "location" => $this->location,
-                "location_type_id" => $this->location_type_id,
-                "location_type" => $this->location_type,
-                "timeframe" => $this->timeframe,
-                "breakdown" => $this->breakdown_name,
-                "format" => $this->format
-            ]
+            'properties' => new IndicatorDataResource($this->resource)
         ];
     }
 }
