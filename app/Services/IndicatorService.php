b985ac4e8198aeb6b94bfe15d73df0e497b2de1a
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-11 14:14:58 -0500

    adds service methods for score and ranking hybrid search results

--- app/Services/IndicatorService.php
+++ app/Services/IndicatorService.php
@@ -108,6 +108,16 @@ class IndicatorService {
 
     }
 
+    /**
+     * 
+     * Takes the search query from a user and turns it into an embedding by submitting to an LLM
+     * 
+     * @param search The search queyry. More useful if sanitized before passing an arg using the EmbeddingTextSanitizer class
+     * 
+     * @return Illuminate\Http\Client\Response Returns a response so the controller handle any errors communicating with the LLM
+
+     */
+
     public static function fetchSearchAsVector(string $search):Response{
 
         return Http::withHeaders([
@@ -118,6 +128,7 @@ class IndicatorService {
                 'input' => "Find information about: " . $search
             ]);
     }
+
 
     public static function queryEmbeddings(string $input_vector, float $threshold, int $limit=20, array | null $indicator_ids = null):Collection{
 
@@ -165,6 +176,108 @@ class IndicatorService {
 
         return $collection;
         
+    }
+
+    public static function querySearch(string $query, array | null $indicator_ids = null ):Collection{
+
+        return Indicator::search($query)
+            ->when($indicator_ids, fn($query)=> $query->whereIn('id', $indicator_ids))
+            ->take(20)
+            ->get();
+
+    }
+
+    /**
+     * 
+     * Scores keyword search results using the RRF formula
+     * 
+     */
+
+    public static function scoreKeywordSearchResults(Collection $keyword_search_results):Collection{
+
+        return $keyword_search_results
+                ->map(function($indicator, $index) {
+
+                    return [
+                        'indicator' => $indicator,
+                        'keyword_rank' => $index + 1,
+                        'keyword_score' => 1 / ($index + 1 + 60)
+                    ];
+
+                })
+                ->keyBy('indicator.id');
+    }
+
+
+    /**
+     * 
+     * Scores semantic search results using the RRF formula
+     * 
+     */
+
+    public static function scoreSemanticSearchResults(Collection $semantic_search_results):Collection{
+
+        return  $semantic_search_results
+                    ->map(function($indicator, $index) {
+                            return [
+                                'indicator' => $indicator,
+                                'semantic_rank' => $index + 1,
+                                'semantic_score' => 1 / ($index + 1 + 60)
+                            ];
+                        })
+                    ->keyBy('indicator.id');
+    }
+
+
+    /**
+     * 
+     * Merges and ranks the semantic and keyword search
+     * 
+     * @param $keyword_search_scored A collection of indicator keyword search results and their rff scores. Use the keyword search results scoring method to get the rff score
+     * 
+     * @param $semantic_search_scored A collection of indicator keyword search results and their rff scores. Use the semantic search results scoring method to get the rff score
+     * 
+     * @return Illuminate\Support\Collection
+     * 
+     */
+
+    public static function rankSearchResults(Collection $keyword_search_scored, Collection $semantic_search_scored){
+
+        $merged = [];
+
+        foreach($keyword_search_scored as $id => $data) {
+            
+            $merged[$id] = [
+                'indicator' => $data['indicator'],
+                'rrf_score' => $data['keyword_score']
+            ];
+
+        }
+
+        foreach($semantic_search_scored as $id => $data) {
+            
+            //if indicator shows up in the keyword search, add the semantic score to it so it shows up higher in the results
+            if(isset($merged[$id])) {
+
+                $merged[$id]['rrf_score'] += $data['semantic_score'];
+
+            } else {
+
+                $merged[$id] = [
+                    'indicator' => $data['indicator'],
+                    'rrf_score' => $data['semantic_score']
+                ];
+
+            }
+
+        }
+
+        // Sort by combined score and take top 10
+        return collect($merged)
+            ->sortByDesc('rrf_score')
+            ->take(10)
+            ->pluck('indicator');
+    
     }
 
 
