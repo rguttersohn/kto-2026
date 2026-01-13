12ffc0a12ce625d064881be8bf613abb1d4fbbae
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-13 12:59:02 -0500

    removes unused geojson transformation method

--- app/Support/GeoJSON.php
+++ app/Support/GeoJSON.php
@@ -6,23 +6,6 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class GeoJSON {
 
-    public static function getGeoJSON(array $data, string $geometry_column):array{
-
-            return  [
-                    'type' => 'FeatureCollection',
-                    'features' => array_map(function($d)use($geometry_column){
-                      
-                        return [
-                            'type' => 'Feature',
-                            'geometry' => json_decode($d[$geometry_column]),
-                            'properties' => array_filter($d, fn($a)=>$a !== $geometry_column, ARRAY_FILTER_USE_KEY)
-                        ];
-
-                    }, $data)
-                ];
-        
-    }
-
     public static function wrapGeoJSONResource(JsonResource $resource){
 
         $features = $resource instanceof ResourceCollection ? $resource : [$resource];
