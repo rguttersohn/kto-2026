d345702e882c51584cc232aa56223e2612eaa046
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-13 12:59:51 -0500

    adds method for retrieving filter ids

--- app/Models/IndicatorData.php
+++ app/Models/IndicatorData.php
@@ -154,6 +154,24 @@ class IndicatorData extends Model
     }
 
     #[Scope]
+    protected function withFilterIDs(Builder $query){
+        
+        return $query    
+                ->select('data.indicator_id')
+                ->selectRaw('array_agg(DISTINCT timeframe ORDER BY timeframe) as timeframes')
+                ->selectRaw('array_agg(DISTINCT lt.id) AS location_types')
+                ->selectRaw('array_agg(DISTINCT df.id) AS data_formats')
+                ->selectRaw('array_agg(DISTINCT COALESCE(bk_parent.id, bk_child.id)) as breakdowns')
+                ->join('locations.locations as l', 'data.location_id', 'l.id')
+                ->join('locations.location_types as lt', 'l.location_type_id', 'lt.id')
+                ->join('indicators.data_formats as df', 'data.data_format_id', 'df.id')
+                ->join('indicators.breakdowns as bk_child', 'data.breakdown_id', 'bk_child.id')
+                ->leftJoin('indicators.breakdowns as bk_parent', 'bk_child.parent_id', 'bk_parent.id')
+                ->groupBy('data.indicator_id');
+
+    }
+
+    #[Scope]
     protected function withDetailsWithOutLimit(
             Builder $query, 
             bool $wants_geojson = false,
