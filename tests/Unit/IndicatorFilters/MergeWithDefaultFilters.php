464b7ce09fee4c746d42fc5f58acdb55e843e0cb
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-13 15:38:28 -0500

    updates test for optional arg that allows excluding filter

---
+++ tests/Unit/IndicatorFilters/MergeWithDefaultFilters.php
@@ -0,0 +1,301 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\IndicatorFiltersFormatter;
+use Tests\TestCase;
+use Mockery;
+
+class MergeWithDefaultFilters extends TestCase
+{
+    
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+
+    
+    public function test_it_uses_request_filter_when_key_exists_in_both_arrays()
+    {
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021', '2022'],
+        ];
+
+        $requestFilters = [
+            'timeframe' => ['eq' => '2020'],
+        ];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals(['timeframe' => ['eq' => '2020']], $result);
+    }
+
+    
+    public function test_uses_last_timeframe_value_when_not_in_request()
+    {
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021', '2022'],
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals(['timeframe' => ['eq' => '2022']], $result);
+    }
+
+    
+    public function test_uses_first_breakdown_id_when_no_sub_breakdowns()
+    {
+        $breakdown = Mockery::mock();
+        $breakdown->id = 123;
+        $breakdown->subBreakdowns = collect([]);
+        
+        $breakdowns = collect([$breakdown]);
+
+        $indicatorFilters = [
+            'breakdown' => $breakdowns,
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals(['breakdown' => ['eq' => 123]], $result);
+    }
+
+    
+    public function test_uses_first_sub_breakdown_id_when_sub_breakdowns_exist()
+    {
+        $subBreakdown = Mockery::mock();
+        $subBreakdown->id = 456;
+
+        $breakdown = Mockery::mock();
+        $breakdown->id = 123;
+        $breakdown->subBreakdowns = collect([$subBreakdown]);
+        
+        $breakdowns = collect([$breakdown]);
+
+        $indicatorFilters = [
+            'breakdown' => $breakdowns,
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals(['breakdown' => ['eq' => 456]], $result);
+    }
+
+    
+    public function test_uses_first_location_type_id_when_not_in_request()
+    {
+        $locationType = Mockery::mock();
+        $locationType->id = 789;
+
+        $locationTypes = collect([$locationType]);
+
+        $indicatorFilters = [
+            'location_type' => $locationTypes,
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals(['location_type' => ['eq' => 789]], $result);
+    }
+
+    
+    public function test_uses_first_format_id_when_not_in_request()
+    {
+        $format = Mockery::mock();
+        $format->id = 999;
+
+        $formats = collect([$format]);
+
+        $indicatorFilters = [
+            'format' => $formats,
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals(['format' => ['eq' => 999]], $result);
+    }
+
+    
+    public function test_handles_multiple_filters_with_mixed_sources()
+    {
+        $breakdown = Mockery::mock();
+        $breakdown->id = 123;
+        $breakdown->subBreakdowns = collect([]);
+
+        $locationType = Mockery::mock();
+        $locationType->id = 789;
+
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021', '2022'],
+            'breakdown' => collect([$breakdown]),
+            'location_type' => collect([$locationType]),
+        ];
+
+        $requestFilters = [
+            'timeframe' => ['eq' => '2021'], // Override
+            // breakdown will use default
+            // location_type will use default
+        ];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals([
+            'timeframe' => ['eq' => '2021'],
+            'breakdown' => ['eq' => 123],
+            'location_type' => ['eq' => 789],
+        ], $result);
+    }
+
+    
+    public function test_returns_empty_array_when_both_inputs_are_empty()
+    {
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters([], []);
+
+        $this->assertEquals([], $result);
+    }
+
+    
+    public function test_handles_all_request_filters_overriding_defaults()
+    {
+        $breakdown = Mockery::mock();
+        $breakdown->id = 123;
+        $breakdown->shouldReceive('getAttribute')
+            ->with('subBreakdowns')
+            ->andReturn(collect([]));
+
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021'],
+            'breakdown' => collect([$breakdown]),
+        ];
+
+        $requestFilters = [
+            'timeframe' => ['eq' => '2019'],
+            'breakdown' => ['eq' => 999],
+        ];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters($indicatorFilters, $requestFilters);
+
+        $this->assertEquals([
+            'timeframe' => ['eq' => '2019'],
+            'breakdown' => ['eq' => 999],
+        ], $result);
+    }
+
+    public function test_it_excludes_timeframe_default_when_specified()
+    {
+        
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021', '2022'],
+            'breakdown' => $this->createBreakdownMock(123),
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters(
+            $indicatorFilters, 
+            $requestFilters,
+            ['timeframe'] // Exclude timeframe from defaults
+        );
+
+        // Should only have breakdown, not timeframe
+        $this->assertEquals(['breakdown' => ['eq' => 123]], $result);
+        $this->assertArrayNotHasKey('timeframe', $result);
+    }
+
+    public function it_excludes_multiple_filters_from_defaults()
+    {
+        $locationType = Mockery::mock();
+        $locationType->id = 789;
+
+        $format = Mockery::mock();
+        $format->id = 999;
+
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021', '2022'],
+            'breakdown' => $this->createBreakdownMock(123),
+            'location_type' => collect([$locationType]),
+            'format' => collect([$format]),
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters(
+            $indicatorFilters, 
+            $requestFilters,
+            ['timeframe', 'location_type'] // Exclude both
+        );
+
+        // Should only have breakdown and format
+        $this->assertEquals([
+            'breakdown' => ['eq' => 123],
+            'format' => ['eq' => 999],
+        ], $result);
+
+        $this->assertArrayNotHasKey('timeframe', $result);
+        $this->assertArrayNotHasKey('location_type', $result);
+    }
+
+    public function test_it_still_uses_request_filters_even_when_excluded_from_defaults()
+    {
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021', '2022'],
+            'breakdown' => $this->createBreakdownMock(123),
+        ];
+
+        $requestFilters = [
+            'timeframe' => ['eq' => '2019'], // User explicitly set this
+        ];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters(
+            $indicatorFilters, 
+            $requestFilters,
+            ['timeframe'] // Even though excluded, request should still apply
+        );
+
+        // Request filter should still be present
+        $this->assertEquals([
+            'timeframe' => ['eq' => '2019'],
+            'breakdown' => ['eq' => 123],
+        ], $result);
+    }
+
+
+    public function it_works_normally_with_empty_exclusion_array()
+    {
+        $indicatorFilters = [
+            'timeframe' => ['2020', '2021', '2022'],
+        ];
+
+        $requestFilters = [];
+
+        $result = IndicatorFiltersFormatter::mergeWithDefaultFilters(
+            $indicatorFilters, 
+            $requestFilters,
+            [] // Empty exclusion array
+        );
+
+        // Should behave like before - apply defaults
+        $this->assertEquals(['timeframe' => ['eq' => '2022']], $result);
+    }
+
+    private function createBreakdownMock($id)
+    {
+        $breakdown = Mockery::mock();
+        $breakdown->id = $id;
+        $breakdown->subBreakdowns = collect([]);
+        
+        return collect([$breakdown]);
+    }
+
+}
