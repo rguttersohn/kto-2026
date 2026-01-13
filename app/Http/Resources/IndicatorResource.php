024f0a28a346963823ccd312e834dd03ac3deb38
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-13 13:05:59 -0500

    adds check for loading of filters relationship

--- app/Http/Resources/IndicatorResource.php
+++ app/Http/Resources/IndicatorResource.php
@@ -5,14 +5,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\IndicatorDataResource;
 use App\Http\Controllers\Traits\HandlesAPIRequestOptions;
 use App\Support\GeoJSON;
-
+use App\Http\Resources\IndicatorBreakdownResource;
 
 class IndicatorResource extends JsonResource
 {
     use HandlesAPIRequestOptions;
 
     public function toArray($request){
-
+        
         return [
             "id" => $this->id,
             "name" => $this->name,
@@ -20,12 +20,20 @@ class IndicatorResource extends JsonResource
             "source" => $this->source,
             "note" => $this->note,
             "data" => $this->whenLoaded('data', function () use ($request) {
+                
                 $wants_geojson = $this->wantsGeoJSON($request);
         
                 return $wants_geojson
                     ? GeoJSON::wrapGeoJSONResource(IndicatorGeoJSONDataResource::collection($this->data))
                     : IndicatorDataResource::collection($this->data);
+
             }),
+            'filters' => $this->whenLoaded('filters', fn()=>[
+                'format' => IndicatorFormatResource::collection($this->filters['data_format']),
+                'location_type' => LocationTypeResource::collection($this->filters['location_type']),
+                'breakdown' => IndicatorBreakdownResource::collection($this->filters['breakdown']),
+                'timeframe' => $this->filters['timeframe']->toArray()
+            ])
         ];
         
     }
