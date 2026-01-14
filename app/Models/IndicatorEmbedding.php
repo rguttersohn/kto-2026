893475872ec1b32dc7e755ef602ba407582ad8d5
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-14 17:38:30 -0500

    removes join method

--- app/Models/IndicatorEmbedding.php
+++ app/Models/IndicatorEmbedding.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Attributes\Scope;
-use Illuminate\Database\Eloquent\Builder;
 
 
 class IndicatorEmbedding extends Model
@@ -20,17 +18,6 @@ class IndicatorEmbedding extends Model
     public function indicators(){
         return $this->belongsTo(Indicator::class);
     }
-
-    #[Scope]
-    protected function joinParents(Builder $query){
-
-        $query->join('indicators.indicators', 'indicators.indicator_embeddings.indicator_id', 'indicators.indicators.id')
-            ->join('indicators.categories', 'indicators.indicators.category_id', 'indicators.categories.id')
-            ->join('domains.domains', 'indicators.categories.domain_id', 'domains.domains.id');
-    
-    }
-
-
 
 
 }
