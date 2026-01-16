0d46187cb3dc00b59ade5dbc8bfa88b0ef7c839f
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-01-16 13:14:15 -0500

    refactors routes to reflect removal of api subdomain

--- routes/web.php
+++ routes/web.php
@@ -1,129 +1,80 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InternalAPIControllers\SearchController;
 use App\Http\Controllers\InternalAPIControllers\IndicatorsController;
 use App\Http\Controllers\InternalAPIControllers\LocationsController;
 use App\Http\Controllers\InternalAPIControllers\LocationTypesController;
-use App\Http\Controllers\PageControllers\IndexController;
-use App\Http\Controllers\PageControllers\IndicatorMapController;
-use App\Http\Controllers\PageControllers\IndicatorQueryController;
-use App\Http\Controllers\PageControllers\IndicatorIndexController;
-use App\Http\Controllers\PageControllers\IndicatorAllController;
-use App\Http\Controllers\PageControllers\CommunityAllController;
-use App\Http\Controllers\PageControllers\CommunityIndexController;
 use App\Http\Controllers\InternalAPIControllers\DomainController;
-use App\Http\Middleware\DevAutoLogin;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 
-Route::get('/', [IndexController::class, 'index']);
+Route::get('/', fn()=>response()->json(['message' => 'welcome']));
 
-Route::group([
-    
-    'prefix' => 'indicators'
-
-], function(){
-
-    Route::get('/', [IndicatorAllController::class, 'index']);
-
-    Route::get('/{indicator_id}', [IndicatorIndexController::class, 'index']);
-
-    Route::get('/{indicator_id}/map', [IndicatorMapController::class, 'index']);
-
-    Route::get('/{indicator_id}/query', [IndicatorQueryController::class, 'index']);
-
-});
-
-
-Route::group([
-
-    'prefix' => 'community-profiles'
-
-], function(){
-    
-    Route::get('/', [CommunityAllController::class, 'index']);
-
-    Route::get('/{location_id}', [CommunityIndexController::class, 'index']);
-});
-
-/**
+/***
  * 
- * API Endpoints
+ * Sanctum
  * 
  */
 
-Route::domain('api.' . config('app.url'))->group(function(){
+Route::get('/sanctum/csrf-cookie', function () {
+    return response()->json(['message' => 'CSRF cookie set']);
+});
+
+
+/**
+ * Internal API
+ */
+
+Route::prefix('app/v1')->group(function(){
+    
+    /***
+     * 
+     * Current User
+     * 
+     */
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user()->toResource();
+    });
 
 
     /***
      * 
-     * Sanctum
+     * Domain Endpoints
      * 
      */
 
-    Route::get('/sanctum/csrf-cookie', function () {
-        return response()->json(['message' => 'CSRF cookie set']);
-    });
-
+    Route::get('domains', [DomainController::class, 'index'])->name('api.app.domain.index');
 
     /**
-     * Internal API
+     *  Indicator end points
      */
 
-    Route::prefix('app/v1')->middleware(DevAutoLogin::class)->group(function(){
-        
-        /***
-         * 
-         * Current User
-         * 
-         */
+    Route::get('indicators', [IndicatorsController::class, 'index'])->name('api.app.indicators.index');
 
-        Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-            return $request->user();
-        });
+    Route::get('indicators/{indicator}', [IndicatorsController::class, 'show'])->name('api.app.indicators.show');
 
+    Route::get('indicators/{indicator}/data', [IndicatorsController::class, 'data'])->name('api.app.indicators.data');
 
-        /***
-         * 
-         * Domain Endpoints
-         * 
-         */
+    Route::get('indicators/{indicator}/data/count', [IndicatorsController::class, 'count'])->name('api.app.indicators.data.count');
 
-        Route::get('domains', [DomainController::class, 'index'])->name('api.app.domain.index');
+    Route::get('indicators/{indicator}/data/export', [IndicatorsController::class, 'export'])->name('api.app.indicators.data.export');
 
-        /**
-         *  Indicator end points
-         */
+    Route::get('indicators/{indicator}/filters', [IndicatorsController::class, 'availableFilters'])->name('api.app.indicators.filters');
 
-        Route::get('indicators', [IndicatorsController::class, 'index'])->name('api.app.indicators.index');
+    /**
+     * Community Profile Endpoints
+     */
 
+    Route::get('location-types', [LocationTypesController::class, 'index'])->name('api.app.location_types.index');
+    
+    Route::get('location-types/{location_type}',[LocationTypesController::class, 'show'])->name('api.app.location_types.show');
 
-        Route::get('indicators/{indicator}', [IndicatorsController::class, 'show'])->name('api.app.indicators.show');
+    Route::get('location-types/{location_type}/indicators',[LocationTypesController::class, 'indicatorIndex'])->name('api.app.location_types.indicators.index');
+    
 
-        Route::get('indicators/{indicator}/data', [IndicatorsController::class, 'data'])->name('api.app.indicators.data');
+    Route::get('location-types/{location_type}/locations', [LocationsController::class, 'index'])->name('api.app.location_types.locations.index');
 
-        Route::get('indicators/{indicator}/data/count', [IndicatorsController::class, 'count'])->name('api.app.indicators.data.count');
+    Route::get('location-types/{location_type}/locations/{location}', [LocationsController::class, 'show'])->name('api.app.location_types.locations.show');
 
-        Route::get('indicators/{indicator}/data/export', [IndicatorsController::class, 'export'])->name('api.app.indicators.data.export');
-
-        Route::get('indicators/{indicator}/filters', [IndicatorsController::class, 'availableFilters'])->name('api.app.indicators.filters');
-
-        /**
-         * Community Profile Endpoints
-         */
-
-        Route::get('location-types', [LocationTypesController::class, 'index'])->name('api.app.location_types.index');
-        
-        Route::get('location-types/{location_type}',[LocationTypesController::class, 'show'])->name('api.app.location_types.show');
-
-        Route::get('location-types/{location_type}/indicators',[LocationTypesController::class, 'indicatorIndex'])->name('api.app.location_types.indicators.index');
-        
-
-        Route::get('location-types/{location_type}/locations', [LocationsController::class, 'index'])->name('api.app.location_types.locations.index');
-
-        Route::get('location-types/{location_type}/locations/{location}', [LocationsController::class, 'show'])->name('api.app.location_types.locations.show');
-
-        
-    });
-
+    
 });
