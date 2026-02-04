adc137785c5a746a1aae7cc87cd331e2c5632fd8
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-02-04 12:03:10 -0500

    fixes issue with indicator form

--- app/Filament/Resources/Indicators/Schemas/IndicatorForm.php
+++ app/Filament/Resources/Indicators/Schemas/IndicatorForm.php
@@ -8,24 +8,38 @@ use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\Select;
 use App\Filament\Support\UIPermissions;
+use Filament\Forms\Components\RichEditor;
 
 class IndicatorForm
-{
+{   
+
     public static function configure(Schema $schema): Schema
     {
         return $schema
             ->components([
                 TextInput::make('name')
                     ->required(),
-                Select::make('category')
+                Select::make('category_id')
                     ->relationship('category', 'name')
                     ->required(),
                 Textarea::make('definition')
                     ->columnSpanFull(),
-                Textarea::make('source')
-                    ->columnSpanFull(),
-                Textarea::make('note')
-                    ->columnSpanFull(),
+                RichEditor::make('source')
+                    ->columnSpanFull()
+                    ->toolbarButtons([
+                        ['bold', 'italic', 'underline', 'strike', 'subscript', 'superscript'],
+                        ['alignStart', 'alignCenter', 'alignEnd'],
+                        ['bulletList', 'orderedList'],
+                        ['undo', 'redo'],
+                    ]),
+                RichEditor::make('note')
+                    ->columnSpanFull()
+                    ->toolbarButtons([
+                        ['bold', 'italic', 'underline', 'strike', 'subscript', 'superscript'],
+                        ['alignStart', 'alignCenter', 'alignEnd'],
+                        ['bulletList', 'orderedList'],
+                        ['undo', 'redo'],
+                    ]),
                 Toggle::make('is_published')
                     ->disabled(fn()=>!UIPermissions::canPublish())
                     ->required(),
