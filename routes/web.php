1adf4ece5163cd76beaa7d829cbd415d77d15719
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-02-04 11:21:31 -0500

    moves all api endpoints to use web middleware guard to check for editor or admin user role

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InternalAPIControllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InternalAPIControllers\IndicatorsController;
 use App\Http\Controllers\InternalAPIControllers\LocationsController;
@@ -36,43 +37,51 @@ Route::prefix('app/v1')->group(function(){
         return $request->user()->toResource();
     });
 
+    Route::middleware(['auth:sanctum', 'web'])->post('/logout', [UserController::class, 'logout']);
 
-    /***
-     * 
-     * Domain Endpoints
-     * 
-     */
 
-    Route::get('domains', [DomainController::class, 'index'])->name('api.app.domain.index');
+    //Should use web middleware for checking if user is an editor or admin
 
-    /**
-     *  Indicator end points
-     */
+    Route::middleware('web')->group(function(){
+        
+        /***
+         * 
+         * Domain Endpoints
+         * 
+         */
+        
+        Route::get('domains', [DomainController::class, 'index'])->name('api.app.domain.index');
 
-    Route::get('indicators', [IndicatorsController::class, 'index'])->name('api.app.indicators.index');
+        /**
+         *  Indicator end points
+         */
 
-    Route::get('indicators/{indicator}', [IndicatorsController::class, 'show'])->name('api.app.indicators.show');
+        Route::get('indicators', [IndicatorsController::class, 'index'])->name('api.app.indicators.index');
 
-    Route::get('indicators/{indicator}/data', [IndicatorsController::class, 'data'])->name('api.app.indicators.data');
+        Route::get('indicators/{indicator}', [IndicatorsController::class, 'show'])->name('api.app.indicators.show');
 
-    Route::get('indicators/{indicator}/data/count', [IndicatorsController::class, 'count'])->name('api.app.indicators.data.count');
+        Route::get('indicators/{indicator}/data', [IndicatorsController::class, 'data'])->name('api.app.indicators.data');
 
-    Route::get('indicators/{indicator}/data/export', [IndicatorsController::class, 'export'])->name('api.app.indicators.data.export');
+        Route::get('indicators/{indicator}/data/count', [IndicatorsController::class, 'count'])->name('api.app.indicators.data.count');
 
-    Route::get('indicators/{indicator}/filters', [IndicatorsController::class, 'availableFilters'])->name('api.app.indicators.filters');
+        Route::get('indicators/{indicator}/data/export', [IndicatorsController::class, 'export'])->name('api.app.indicators.data.export');
 
-    /**
-     * Location Endpoints
-     */
+        Route::get('indicators/{indicator}/filters', [IndicatorsController::class, 'availableFilters'])->name('api.app.indicators.filters');
 
-    Route::get('location-types', [LocationTypesController::class, 'index'])->name('api.app.location_types.index');
+        /**
+         * Location Endpoints
+         */
+
+        Route::get('location-types', [LocationTypesController::class, 'index'])->name('api.app.location_types.index');
+        
+        Route::get('location-types/{location_type}',[LocationTypesController::class, 'show'])->name('api.app.location_types.show');
+        
+        Route::get('location-types/{location_type}/locations', [LocationsController::class, 'index'])->name('api.app.location_types.locations.index');
+
+        Route::get('locations/{location}', [LocationsController::class, 'show'])->name('api.app.locations.show');
+
+        Route::get('locations/{location}/indicators', [LocationsController::class, 'indicatorIndex'])->name('api.app.location.indicators.index');
     
-    Route::get('location-types/{location_type}',[LocationTypesController::class, 'show'])->name('api.app.location_types.show');
-    
-    Route::get('location-types/{location_type}/locations', [LocationsController::class, 'index'])->name('api.app.location_types.locations.index');
-
-    Route::get('locations/{location}', [LocationsController::class, 'show'])->name('api.app.locations.show');
-
-    Route::get('locations/{location}/indicators', [LocationsController::class, 'indicatorIndex'])->name('api.app.location.indicators.index');
+    });
 
 });
