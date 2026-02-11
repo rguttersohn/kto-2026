3aebb82e047760b283db3af44cba50fffaea93b0
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-02-11 13:06:41 -0500

    adds check and validation of filter from request

--- app/Http/Controllers/InternalAPIControllers/LocationTypesController.php
+++ app/Http/Controllers/InternalAPIControllers/LocationTypesController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\LocationTypeResource;
 use App\Http\Controllers\Traits\HandlesAPIRequestOptions;
 use App\Http\Controllers\Controller;
 use App\Services\LocationService;
+use Illuminate\Http\Request;
 
 
 class LocationTypesController extends Controller
@@ -15,9 +16,11 @@ class LocationTypesController extends Controller
     use HandlesAPIRequestOptions;
 
 
-    public function index(){
+    public function index(Request $request){
+        
+        $filters = $this->filters($request);
 
-        $location_types = LocationService::queryAllLocationTypes();
+        $location_types = LocationService::queryAllLocationTypes(filters: $filters);
 
         return response()->json([
             'data' => LocationTypeResource::collection($location_types)
