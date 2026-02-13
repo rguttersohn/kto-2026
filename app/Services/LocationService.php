2945cc431e9ea69f3367b933be2528da9478df73
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-02-13 17:04:11 -0500

    adds uninhabited to select list

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -20,7 +20,7 @@ class LocationService {
     }
 
     public static function queryLocationsByLocationType(int $location_type_id, ?bool $wants_geojson = false):Collection | null{
-        return Location::select('location_type_id','name','locations.id','fips','district_id')
+        return Location::select('location_type_id','name','locations.id','fips','district_id','is_uninhabited')
             ->where('location_type_id', $location_type_id)
             ->when($wants_geojson, function($query){
 
@@ -38,7 +38,7 @@ class LocationService {
         return Location::where('locations.id', $location_id)
             ->when($wants_geojson, function($query){
 
-                $query->join('locations.geometries as geo', 'locations.id', 'geo.location_id')
+                $query->join('locations.geometries as geo', 'locations.id', 'geo.location_id','is_uninhabited')
                     ->selectRaw(PostGIS::getSimplifiedGeoJSON('geo','geometry'));
 
             })
@@ -56,7 +56,7 @@ class LocationService {
 
         return LocationType::select('id', 'name', 'plural_name','scope', 'classification')
             ->with(['locations' => function($query)use($wants_geojson){
-                $query->select('location_type_id', 'name','locations.id','fips','district_id')
+                $query->select('location_type_id', 'name','locations.id','fips','district_id','is_uninhabited')
                     ->when($wants_geojson, function($query){
                         $query->join('locations.geometries as geo', 'locations.id', 'geo.location_id')
                             ->selectRaw(PostGIS::getSimplifiedGeoJSON('geo','geometry'))
