37a435633c9d07c0a6c84fcb3e0639c4e33c00c0
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-02-19 11:42:34 -0500

    updates vite config to remove vue and inertia

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path';
 
 export default defineConfig({
     plugins: [
-        VueDevTools({
-            appendTo: "app.ts",
-        }),
-        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            input: ['resources/css/app.css'],
             refresh: true,
         }),
         tailwindcss(),
-    ],
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
-      },
-      alias: {
-        '@services': path.resolve(__dirname, './resources/js/services')
-      },
+    ]
 });
