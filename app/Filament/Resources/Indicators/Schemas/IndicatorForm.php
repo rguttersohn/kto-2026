ca17d10e6739e616c232c74bf3b4a0e682791a90
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-03-13 10:47:23 -0400

    adds methods that retrieve default filter select options

--- app/Filament/Resources/Indicators/Schemas/IndicatorForm.php
+++ app/Filament/Resources/Indicators/Schemas/IndicatorForm.php
@@ -9,9 +9,112 @@ use Filament\Schemas\Schema;
 use Filament\Forms\Components\Select;
 use App\Filament\Support\UIPermissions;
 use Filament\Forms\Components\RichEditor;
+use Filament\Schemas\Components\Section;
+use App\Filament\Services\AdminIndicatorService;
+use App\Models\Breakdown;
+use App\Models\Location;
+use App\Models\IndicatorData;
+use Filament\Schemas\Components\Grid;
 
 class IndicatorForm
 {   
+
+    protected static function getBreakdownOptions($record){
+
+        $indicator_id = $record->id;
+
+        return AdminIndicatorService::rememberFilter($indicator_id, 'breakdown',
+
+                function()use($indicator_id){
+                    return Breakdown::select('breakdowns.name', 'breakdowns.id')
+                        ->join('indicators.data', 'breakdowns.id', '=', 'indicators.data.breakdown_id')
+                        ->where('indicators.data.indicator_id', $indicator_id)
+                        ->distinct()
+                        ->pluck('name', 'id');
+
+            });
+    }
+
+    protected static function getLocationTypeOptions($record){
+
+        $indicator_id = $record->id;
+
+        return AdminIndicatorService::rememberFilter($indicator_id, 'location_type', 
+
+                function()use($indicator_id){
+                    
+                    return Location::select('locations.location_types.name', 'locations.location_types.id')
+                        ->join('indicators.data', 'indicators.data.location_id', 'locations.locations.id')
+                        ->join('locations.location_types', 'locations.locations.location_type_id', 'locations.location_types.id')
+                        ->where('indicators.data.indicator_id', $indicator_id)
+                        ->distinct()
+                        ->pluck('name','id');
+                }
+        );
+
+    }
+
+    protected static function getLocationOptions($get, $record){
+
+        $indicator_id = $record->id;
+
+        $location_type_id = $get('location_type_id');
+
+        return AdminIndicatorService::rememberFilter($indicator_id, "location", 
+
+                function()use($indicator_id, $location_type_id){
+                    
+                    return Location::select('locations.locations.name', 'locations.locations.id')
+                        ->join('indicators.data', 'indicators.data.location_id', 'locations.locations.id')
+                        ->where('indicators.data.indicator_id', $indicator_id)
+                        ->when($location_type_id, function($query)use($location_type_id){
+
+                            return $query->where('location_type_id', $location_type_id);
+                        })
+                        ->distinct()
+                        ->pluck('name','id');
+                },
+                "$location_type_id"
+        );
+    }
+
+    protected static function getTimeframeOptions($record){
+
+        $indicator_id = $record->id;
+
+        $filters = AdminIndicatorService::rememberFilter($indicator_id, 'timeframe',
+            
+            function()use($indicator_id){
+
+                return IndicatorData::where('indicator_id', $indicator_id)
+                    ->select('timeframe')
+                    ->distinct()
+                    ->orderBy('timeframe', 'desc')
+                    ->pluck('timeframe','timeframe');
+
+            });
+
+        return $filters;
+    }
+
+    protected static function getFormatOptions($record){
+
+        $indicator_id = $record->id;
+
+        $filter = AdminIndicatorService::rememberFilter($indicator_id, 'format',
+
+            function()use($indicator_id){
+
+                return IndicatorData::select('indicators.data_formats.id as id', 'indicators.data_formats.name as name')
+                    ->where('indicator_id', $indicator_id)
+                    ->join('indicators.data_formats', 'indicators.data.data_format_id', 'indicators.data_formats.id')
+                    ->distinct()
+                    ->pluck('name', 'id');
+            }
+        );
+        return $filter;
+
+    }
 
     public static function configure(Schema $schema): Schema
     {
@@ -48,7 +151,48 @@ class IndicatorForm
                 Toggle::make('is_archived')
                     ->disabled(fn()=>!UIPermissions::canPublish())
                     ->required(),
-
+                Section::make('SEO and Meta')
+                    ->columnSpanFull()
+                    ->relationship('meta')
+                    ->schema([
+                        TextInput::make('meta_title'),
+                        TextInput::make('meta_description'),
+                        TextInput::make('og_title'),
+                        TextInput::make('og_description'),
+                    ]),
+                Section::make('Default Filters')
+                    ->columnSpanFull()
+                    ->relationship('defaultFilters')
+                    ->schema(fn($record)=>[
+                        Grid::make([
+                            'default' => 1,
+                            'lg' => 3
+                        ])
+                        ->schema([
+                            Select::make('timeframe')
+                                ->label('Select Timeframe Filter')
+                                ->options(fn()=>self::getTimeframeOptions($record))
+                                ->searchable(),
+                            Select::make('data_format_id')
+                                ->label('Select Data Format Filter')
+                                ->options(fn()=>self::getFormatOptions($record))
+                                ->searchable(),
+                            Select::make('breakdown_id')
+                                ->label('Select Breakdown Filter')
+                                ->options(fn()=>self::getBreakdownOptions($record))
+                                ->searchable(),
+                            Select::make('location_type_id')
+                                ->label('Select Location Type Filter')
+                                ->options(self::getLocationTypeOptions($record))
+                                ->searchable()
+                                ->live(),
+                            Select::make('location_id')
+                                ->label('Select Location Filter')
+                                ->options(fn($get)=>self::getLocationOptions($get, $record))
+                                ->searchable()
+                                ->live()
+                        ])
+                    ])
             ]);
     }
 }
