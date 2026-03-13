babd6f8590e0ede8d72962c2b83847215953876a
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-03-12 22:17:11 -0400

    adds IndicatorMeta model

---
+++ app/Models/IndicatorMeta.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IndicatorMeta extends Model
+{
+    protected $connection = 'supabase';
+
+    protected $table = 'indicators.meta';
+
+    protected $fillable = [
+        'indicator_id',
+        'meta_title',
+        'meta_description',
+        'og_title',
+        'og_description',
+        'og_image'
+    ];
+
+}
