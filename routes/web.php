26f99d8b351a844056c4d881848dd2168b1af60b
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-04-08 09:29:14 -0400

    adds a with-data endpoint to locations

--- routes/web.php
+++ routes/web.php
@@ -82,6 +82,8 @@ Route::prefix('app/v1')->group(function(){
 
         Route::get('locations/{location}/indicators', [LocationsController::class, 'indicatorIndex'])->name('api.app.location.indicators.index');
 
+        Route::get('locations/{location}/indicators/with-data', [LocationsController::class, 'indicatorIndexWithData'])->name('api.app.location.indicators.index.data');
+
         Route::get('locations/{location}/peers', [LocationsController::class, 'peersIndex'])->name('api.app.location.peers.index');
     
     });
