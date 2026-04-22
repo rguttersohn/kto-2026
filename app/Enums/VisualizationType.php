9eaa7e9f6afcebdd0aa6ae91cd3de485d9f72c15
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2026-04-22 13:50:11 -0400

    adds excluded default filters method that returns array of filters to exclude

--- app/Enums/VisualizationType.php
+++ app/Enums/VisualizationType.php
@@ -18,4 +18,22 @@ enum VisualizationType: string
             self::Card => 'Card',
         };
     }
+
+    /**
+     * 
+     * Returns an array of filters that should be excluded from the default filters for the indicator based on the visualization type. 
+     * This is used to ensure that the default filters do not include filters that are not relevant to the visualization type. 
+     * For example, a line chart may not have a breakdown filter, so it should be excluded from the default filters for indicators with a line chart visualization type.
+     *
+    */
+    public function excludedDefaultFilters(): array
+    {
+        return match($this) {
+            self::Line => ['timeframe'],
+            self::Bar  => ['breakdown'],
+            self::Pie  => ['breakdown'],
+            self::Card => [],
+            default => ['timeframe']
+        };
+    }
 }
